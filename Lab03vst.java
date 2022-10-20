4638269250cc134ffbd41b9e3ff02d5230cd7608
Mr. Kiedes <akiedes@vtsd.com>
2022-10-20 10:35:16 -0400

    Conversion Example

--- Lab03vst.java
+++ Lab03vst.java
@@ -9,7 +9,14 @@ public class Lab03vst
 	public static void main(String[] args)
 	{
       System.out.println("Lab03, 80 Point Version\n");
-      
+      int seconds = 10000;
+	  System.out.println("Seconds: " + seconds);
+
+	  int hours = seconds / 3600;
+	  System.out.println("Hours: " + hours);
+
+	  seconds %= 3600;
+	  System.out.println("Seconds: " + seconds);
 	}
 }
 
